31a8b32b6d7deabe349d27eb60aa70df275ce1d0
Josep M Sobrepere <jm.sobrepere@gmail.com>
2021-09-24 10:24:28 +0200

    fix: persistLocally with different versions

--- src/services/utils/persistLocally.ts
+++ src/services/utils/persistLocally.ts
@@ -1,5 +1,16 @@
 import { Observable } from "rxjs"
 
+const VERSION_KEY = "@smoldot-ads/schema-version"
+const CURRENT_VERSION = 1
+
+if (typeof window !== "undefined") {
+  const version = Number(window.localStorage.getItem(VERSION_KEY))
+  if (version !== CURRENT_VERSION) {
+    window.localStorage.clear()
+    window.localStorage.setItem(VERSION_KEY, CURRENT_VERSION.toString(10))
+  }
+}
+
 export const persistLocally =
   (key: string) =>
   <T>(source: Observable<T>) =>
