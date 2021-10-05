1d6220816c03a26312ed7da2565b2b8297c4a6df
wirednkod <wirednkod@gmail.com>
2021-10-05 18:13:02 +0300

    add new string utils

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -1,2 +1,11 @@
 export const isEmptyText = (input: string): boolean =>
   input.trimStart().trimEnd().length === 0
+
+export const makeEllipsis = (some: string) =>
+  some
+    .slice(0, 4)
+    .concat("...")
+    .concat(some.slice(some.length - 4))
+
+export const capitalize = (some: string) =>
+  some.charAt(0).toUpperCase() + some.slice(1)
